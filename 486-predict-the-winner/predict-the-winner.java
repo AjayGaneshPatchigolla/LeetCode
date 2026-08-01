8ed7b248e0d795489c57aecfccbbf5ca0983c288
Ajay Ganesh <84365759+AjayGaneshPatchigolla@users.noreply.github.com>
2026-08-02 00:57:12 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (41.06%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.java
+++ 486-predict-the-winner/predict-the-winner.java
@@ -1,13 +1,19 @@
 class Solution {
-    static int rec(int i,int j,int nums[])
+    static int rec(int i,int j,int nums[],int dp[][])
     {
         if(i>j)
         return 0;
-        int s1=nums[i]-rec(i+1,j,nums);
-        int s2=nums[j]-rec(i,j-1,nums);
-        return Math.max(s1,s2);
+        if(dp[i][j]!=-1)
+        return dp[i][j];
+        int s1=nums[i]-rec(i+1,j,nums,dp);
+        int s2=nums[j]-rec(i,j-1,nums,dp);
+        return dp[i][j]=Math.max(s1,s2);
     }
     public boolean predictTheWinner(int[] nums) {
-        return rec(0,nums.length-1,nums)>=0;
+        int n=nums.length;
+        int dp[][]=new int[n][n];
+        for(int i[]: dp)
+        Arrays.fill(i,-1);
+        return rec(0,nums.length-1,nums,dp)>=0;
     }
 }
