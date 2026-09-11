e67fdd440d0ac91f9b61f3819c4f24ea6cf82b97
Ajay Ganesh <84365759+AjayGaneshPatchigolla@users.noreply.github.com>
2026-09-11 20:54:09 +0530

    Time: 229 ms (5.07%) | Memory: 46.9 MB (15.20%) - LeetSync

---
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int totalNumbers(int[] digits) {
+        int n=digits.length;
+        HashSet<Integer> hs=new HashSet<>();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                for(int k=0;k<n;k++)
+                {
+                    if(i!=j && j!=k && i!=k)
+                    {
+                        int val=digits[i]*100+digits[j]*10+digits[k];
+                        System.out.println(val);
+                        if(val%2==0 && digits[i]>0){
+                        hs.add(val);
+                        // System.out.println(val+"*");
+                        }
+                    }
+                }
+            }
+        }
+        return hs.size();
+    }
+}
