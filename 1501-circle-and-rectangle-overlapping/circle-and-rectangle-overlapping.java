680ddfcde878f0edb11c226664b02a2bcdfc6e52
Ajay Ganesh <84365759+AjayGaneshPatchigolla@users.noreply.github.com>
2026-09-19 12:30:25 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (22.69%) - LeetSync

--- 1501-circle-and-rectangle-overlapping/circle-and-rectangle-overlapping.java
+++ 1501-circle-and-rectangle-overlapping/circle-and-rectangle-overlapping.java
@@ -4,40 +4,18 @@ class Solution {
         return Math.sqrt(Math.pow(y2-y1,2)+Math.pow(x2-x1,2));
     }
     public boolean checkOverlap(int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2) {
-        for(int i=x1;i<=x2;i++)
-        {
-            double dist=distance(i,y1,xCenter,yCenter); 
-            if(dist<=radius)
-            return true;
-        }
-        for(int i=x1;i<=x2;i++)
-        {
-            double dist=distance(i,y2,xCenter,yCenter); 
-            if(dist<=radius)
-            return true;
-        }
-        for(int i=y1;i<=y2;i++)
-        {
-            double dist=distance(x1,i,xCenter,yCenter); 
-            if(dist<=radius)
-            return true;
-        }
-        for(int i=y1;i<=y2;i++)
-        {
-            double dist=distance(x2,i,xCenter,yCenter); 
-            if(dist<=radius)
-            return true;
-        }
-        while(true)
-        {
-            x1+=1;
-            y1+=1;
-            if(x1>x2  || y1>y2)
-            break;
-            double dist=distance(x1,y1,xCenter,yCenter);
-            if(dist<=radius)
-            return true;
-        }
+        int closeX=xCenter,closeY=yCenter;
+        if(xCenter<x1)
+        closeX=x1;
+        else if(xCenter>x2)
+        closeX=x2;
+        if(yCenter<y1)
+        closeY=y1;
+        else if(yCenter>y2)
+        closeY=y2;
+        double dist=distance(closeX,closeY,xCenter,yCenter);
+        if(dist<=radius)
+        return true;
         return false;
     }
 }
