63ada82358b7c86a68343cb8b3279e3cdc698230
Ajay Ganesh <84365759+AjayGaneshPatchigolla@users.noreply.github.com>
2026-09-25 20:56:27 +0530

    Time: 20 ms (94.58%) | Memory: 48.7 MB (93.00%) - LeetSync

--- 1572-subrectangle-queries/subrectangle-queries.java
+++ 1572-subrectangle-queries/subrectangle-queries.java
@@ -1,21 +1,35 @@
 class SubrectangleQueries {
 
     int rectangle[][];
+    List<Pair> operations;
+    static class Pair
+    {
+        int x1,y1,x2,y2,v;
+        Pair(int x1,int y1,int x2,int y2,int v)
+        {
+            this.x1=x1;
+            this.y1=y1;
+            this.x2=x2;
+            this.y2=y2;
+            this.v=v;
+        }
+    }
     public SubrectangleQueries(int[][] rectangle) {
+        operations=new ArrayList<>();
         this.rectangle=rectangle;
     }
     
     public void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {
-        for(int i=row1;i<=row2;i++)
-        {
-            for(int j=col1;j<=col2;j++)
-            {
-                rectangle[i][j]=newValue;
-            }
-        }
+        operations.add(new Pair(row1,col1,row2,col2,newValue));
     }
     
     public int getValue(int row, int col) {
+        for(int i=operations.size()-1;i>=0;i--)
+        {
+            Pair t=operations.get(i);
+            if(row>=t.x1 && row<=t.x2 && col>=t.y1 && col<=t.y2)
+            return t.v;
+        }
         return rectangle[row][col];
     }
 }
